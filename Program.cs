8de4efe284f9638b0a2719b92304d827cb744f44
Xenorsek <44492935+Xenorsek@users.noreply.github.com>
2020-01-18 12:35:37 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace Projekt
         public class Hotel
         {
             public Osoba Kierownik;
-            public List<Room> pokoje;
+            public List<Room> pokoje = new List<Room>();
             public List<Pracownik> Personel;
             public List<Gosc> Goscie;
 
@@ -141,6 +141,25 @@ namespace Projekt
             }
 
         }
+        class Dostosuj : PokojDekorator
+        {
+            public Dostosuj(Room _room) : base(_room)
+            {
+
+            }
+            public override double Koszt()
+            {
+                return room.Koszt();
+            }
+            public override string GetOpis()
+            {
+                return room.GetOpis();
+            }
+            public override int NumerPokoju()
+            {
+                return room.NumerPokoju() + 1;
+            }
+        }
         class Zwierzeta : PokojDekorator
         {
             public Zwierzeta(Room _room) : base(_room)
@@ -200,71 +219,8 @@ namespace Projekt
                 else
                     return room.NumerPokoju() + 30;
             }
+            
         }
-
-        }
-        class Zwierzeta : PokojDekorator
-        {
-            public Zwierzeta(Room _room) : base(_room)
-            {
-
-            }
-            public override double Koszt()
-            {
-                return 100 + room.Koszt();
-            }
-            public override string GetOpis()
-            {
-                return room.GetOpis() + ", przeznaczony dla zwierząt";
-            }
-            public override int NumerPokoju()
-            {
-                return room.NumerPokoju() + 1;
-            }
-        }
-        class Osobowy : PokojDekorator
-        {
-            public int IloscOsob = 0;
-            public Osobowy(Room _room, int osoby) : base(_room)
-            {
-                this.IloscOsob = osoby;
-            }
-            public override double Koszt()
-            {
-                if (IloscOsob == 1)
-                    return 0 + room.Koszt();
-                else if (IloscOsob == 2)
-                    return 100 + room.Koszt();
-                else if (IloscOsob == 3)
-                    return 150 + room.Koszt();
-                else
-                    return 250 + room.Koszt();
-            }
-            public override string GetOpis()
-            {
-                if (IloscOsob == 1)
-                    return room.GetOpis() + ", Jednoosobowy";
-                else if (IloscOsob == 2)
-                    return room.GetOpis() + ", Dwuosobowy";
-                else if (IloscOsob == 3)
-                    return room.GetOpis() + ", Trzyosobowy";
-                else
-                    return room.GetOpis() + ", Czteroosobowy";
-            }
-            public override int NumerPokoju()
-            {
-                if (IloscOsob == 1)
-                    return room.NumerPokoju() + 0;
-                else if (IloscOsob == 2)
-                    return room.NumerPokoju() + 10;
-                else if (IloscOsob == 3)
-                    return room.NumerPokoju() + 20;
-                else
-                    return room.NumerPokoju() + 30;
-            }
-        }
-
-
         public class Osoba : Hotel
         {
             public string Imie;
@@ -276,7 +232,8 @@ namespace Projekt
             public int IloscOdwiedzin = 0;
             public double Rachunek = 0;
             public int Posilki = 0; // 1 sniadanie 2 obiad 4 kolacja
-            public Gosc(bool czykarta,string imie,string nazwisko)
+            public Room Aktualny_pokoj;
+            public Gosc(bool czykarta, string imie, string nazwisko)
             {
                 this.KartaCzłonkowska = czykarta;
                 this.IloscOdwiedzin = this.IloscOdwiedzin + 1;
@@ -295,9 +252,35 @@ namespace Projekt
                     Console.WriteLine("Zostałeś członkiem klubu");
                 }
             }
-            public void PrzypiszDoPokoju(Pokoj pokoj)
+            public void PrzypiszDoPokoju(Room pokoj)
             {
+                pokoj = PokojDostepny(pokoj);
+                Aktualny_pokoj = pokoj;
+                pokoje.Add(pokoj);
+                if(KartaCzłonkowska == true)
+                    Rachunek = pokoj.Koszt() * 0.80;
+                else
+                Rachunek = pokoj.Koszt();
+                Console.WriteLine("Numer pokoju: " + pokoj.NumerPokoju() + " Koszt: " + Rachunek);
+            }
+            public Room PokojDostepny(Room pokoj)
+            {
+                int numerek = pokoj.NumerPokoju();
+                if (pokoje == null)
+                {
 
+                }
+                else
+                {
+                    foreach (Room p in pokoje)
+                    {
+                        if (numerek == p.NumerPokoju())
+                        {
+                            pokoj = new Dostosuj(pokoj);
+                        }
+                    }
+                }
+                return pokoj;
             }
         }
         public class Pracownik : Osoba
@@ -310,12 +293,12 @@ namespace Projekt
         }
         public class Boj : Pracownik
         {
-            public Boj(string Imię,bool zmianaa,double staż,string nazwisko)
+            public Boj(string Imię, bool zmianaa, double staż, string nazwisko)
             {
                 this.Zmiana = zmianaa;
                 this.Ilosc_Godzin = 56;
                 this.Staż = staż;
-                this.Wyplata += this.Wyplata*this.Ilosc_Godzin + 100 * (Staż / 6);
+                this.Wyplata += this.Wyplata * this.Ilosc_Godzin + 100 * (Staż / 6);
                 this.obowiązki = "Zamawianie taksówki, udzielanie informacji o okolicy, dostarczanie zamawiane do pokoju posiłki czy odnoszenie garderoby do pralni";
                 this.Imie = Imię;
                 this.Nazwisko = nazwisko;
@@ -323,7 +306,7 @@ namespace Projekt
             public void Info()
             {
                 string zminy;
-                if(this.Zmiana == false)
+                if (this.Zmiana == false)
                 {
                     zminy = "Dzienna";
                 }
@@ -344,7 +327,7 @@ namespace Projekt
                 this.Zmiana = zmianaa;
                 this.Ilosc_Godzin = 48;
                 this.Staż = staż;
-                this.Wyplata += this.Wyplata*this.Ilosc_Godzin + 70 * (Staż / 6);
+                this.Wyplata += this.Wyplata * this.Ilosc_Godzin + 70 * (Staż / 6);
                 this.obowiązki = "Dbanie o porządek na obszarze całego hotelu";
                 this.Imie = Imię;
                 this.Nazwisko = nazwisko;
@@ -372,7 +355,7 @@ namespace Projekt
                 this.Zmiana = zmianaa;
                 this.Ilosc_Godzin = 84;
                 this.Staż = staż;
-                this.Wyplata += this.Wyplata*Ilosc_Godzin + 150 * (Staż / 6);
+                this.Wyplata += this.Wyplata * Ilosc_Godzin + 150 * (Staż / 6);
                 this.obowiązki = "Rozporządzanie pokojami hotelowymi i dbanie o przyjemny pobyt klientów";
                 this.Imie = Imię;
                 this.Nazwisko = nazwisko;
@@ -397,14 +380,25 @@ namespace Projekt
         {
 
         }
-        static void Main(string[] args)
-        {
-            Boj jeden = new Boj("Andrzej",false,12,"Dudeł");
-            Sprzątaczka dwa = new Sprzątaczka("Wiesia", false, 2, "Mozambik");
-            Recepcjonista trzy = new Recepcjonista("Mystosław", true, 18, "Bajkiewicz");
+    
+    static void Main(string[] args)
+    {
+        Boj jeden = new Boj("Andrzej", false, 12, "Dudeł");
+        Sprzątaczka dwa = new Sprzątaczka("Wiesia", false, 2, "Mozambik");
+        Recepcjonista trzy = new Recepcjonista("Mystosław", true, 18, "Bajkiewicz");
             //trzy.Info(); //Informacje o pracowniku
-            Console.ReadKey();
-        }
-        //Trzeba zrobić jakiś scenariusz może w mainie porobić cmd symulacji typu zapisz gościa, zatrudnij pracownika, Poproś o rachunek, płatność w ratach xd
+            Room room = new Pokoj();
+            room = new Zwierzeta(room);
+            room = new Osobowy(room, 3);
+            Room room2 = new Pokoj();
+            room2 = new Zwierzeta(room2);
+            room2 = new Osobowy(room2, 3);
+            Gosc osoba = new Gosc(true, "Marcin", "Kozlowski");
+            osoba.PrzypiszDoPokoju(room);
+            osoba.PrzypiszDoPokoju(room2);
+            Console.WriteLine(osoba.Rachunek);
+        Console.ReadKey();
     }
+    //Trzeba zrobić jakiś scenariusz może w mainie porobić cmd symulacji typu zapisz gościa, zatrudnij pracownika, Poproś o rachunek, płatność w ratach xd
+}
 }
